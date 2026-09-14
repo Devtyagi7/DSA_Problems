bb35378071178de254421a52d22a1ea097c8db08
Devtyagi7 <devt3650@gmail.com>
2026-09-14 22:17:40 +0530

    Time: 54 ms (41.26%), Space: 116.5 MB (19.93%) - LeetHub

---
+++ 1944-number-of-visible-people-in-a-queue/1944-number-of-visible-people-in-a-queue.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] canSeePersonsCount(int[] arr) {
+        int n=arr.length;
+        int[] ans=new int[n];
+        Stack<Integer> st=new Stack<>();
+        ans[n-1]=0;
+        st.push(arr[n-1]);
+        for(int i=n-2; i>=0; i--){
+            int count=0;
+            int value=arr[i];
+            while(st.size()>0 && st.peek() <= value){
+                st.pop();
+                count++;
+            }
+            if(st.size()>0) count++;
+            ans[i] = count;
+            st.push(arr[i]);
+        } 
+        return ans;   
+    }
+}
